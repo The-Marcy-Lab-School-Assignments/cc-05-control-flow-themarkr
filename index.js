564f90c1506772b2882ab8269a0cf65b173f1fc7
Mark Rodriguez <markrodriguezaa@gmail.com>
2021-10-06 14:24:38 +0000

    completed problem and bonus problem

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 //1
-function absoluteValue(){
-  
+function absoluteValue(num){
+  if(isNaN(num) === false){
+      if(num > 0){
+          return num;
+      }else{
+          return num*-1;
+      }
+  }else{
+      return null;
+  }
 }
 
+console.log(absoluteValue(-3));
 
+//bonus
+function multiplesOfFourAndSix() {
+    let multiples = [];
+    for (let i = 1; i<=100; i++){
+        if (i%4 === 0 && i%6 === 0){
+            multiples.push(i);
+        }
+    }
+    return multiples;
+}
 
-
+console.log(multiplesOfFourAndSix());
